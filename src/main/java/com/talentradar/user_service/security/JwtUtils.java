16a9045c2a1cfd7262255e470a2f7e57a293759b
ericndungutse <dav.ndungutse@gmail.com>
2025-07-15 23:06:29 +0200

    feat(tra-8): remove unused codes and import in jwtutils class

--- src/main/java/com/talentradar/user_service/security/JwtUtils.java
+++ src/main/java/com/talentradar/user_service/security/JwtUtils.java
@@ -3,8 +3,6 @@ package com.talentradar.user_service.security;
 import java.security.Key;
 import java.util.Date;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
@@ -14,33 +12,15 @@ import com.talentradar.user_service.model.CustomUserDetails;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import jakarta.servlet.http.HttpServletRequest;
 
 @Component
 public class JwtUtils {
-    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
     @Value("${spring.security.jwt.secret}")
     private String jwtSecret;
 
     @Value("${spring.security.jwt.expirationMs}")
     private String jwtExpirationMS;
-
-    // Get Jwt from header of the request
-    public String getJwtFromHeader(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-
-        logger.info("Authorization Header: { }", bearerToken);
-        if (bearerToken == null) {
-            return null;
-        }
-
-        String token = bearerToken.substring(7);
-
-        logger.info("Token: { }", token);
-
-        return token;
-    }
 
     // Generate Jwt token
     public String generateJwtTokenFromEmail(CustomUserDetails userDetails) {
