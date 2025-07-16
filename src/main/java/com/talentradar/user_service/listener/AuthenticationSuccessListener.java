8b3ed463f090e83befa4806d31abc3bd87bacb99
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-07-16 13:50:16 +0200

    feat: create session and store user metadata
    This change will create the actual session on redis and store some
    other information about the user on postgres DB

---
+++ src/main/java/com/talentradar/user_service/listener/AuthenticationSuccessListener.java
@@ -0,0 +1,61 @@
+package com.talentradar.user_service.listener;
+
+import com.talentradar.user_service.exception.UserNotFoundException;
+import com.talentradar.user_service.model.Session;
+import com.talentradar.user_service.model.User;
+import com.talentradar.user_service.repository.SessionRepository;
+import com.talentradar.user_service.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.time.LocalDateTime;
+
+/* The class handles the event upon successful sign-in */
+@Component
+public class AuthenticationSuccessListener implements ApplicationListener<AuthenticationSuccessEvent> {
+
+    private final SessionRepository sessionRepository;
+    private final UserRepository userRepository;
+
+    public AuthenticationSuccessListener(SessionRepository sessionRepository, UserRepository userRepository) {
+        this.sessionRepository = sessionRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(AuthenticationSuccessEvent event) {
+        String email = ((UserDetails) event.getAuthentication().getPrincipal()).getUsername();
+        // check whether username exists
+        User user = this.userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException(
+                        String.format("A user with the email '%s' does not exist",
+                                email))
+                );
+
+        HttpServletRequest request = ((ServletRequestAttributes)
+                RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        String ip = request.getRemoteAddr();
+        String userAgent = request.getHeader("User-Agent");
+
+        String sessionId = request.getSession().getId();
+
+        Session session = Session.builder()
+                .sessionId(sessionId)
+                .id(user.getId())
+                .ipAddress(ip)
+                .deviceInfo(userAgent)
+                .loginTimestamp(LocalDateTime.now())
+                .isActive(true)
+                .build();
+
+        sessionRepository.save(session);
+    }
+}
+
+
---
+++ src/main/java/com/talentradar/user_service/repository/SessionRepository.java
@@ -0,0 +1,8 @@
+package com.talentradar.user_service.repository;
+
+import com.talentradar.user_service.model.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+    //TODO: retrieve or find sessions By userId and session is active
+}
